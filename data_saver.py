2702a06b8b6df18dbe6ac7e5716d7c0a666d68c8
j.lamin@sanadtech.com <jawadlamin83@gmail.com>
2025-03-08 22:00:29 +0000

    add upload to drive function

--- data_saver.py
+++ data_saver.py
@@ -2,6 +2,11 @@ import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("savedata-452919-69fd401a8a6d.json", scope)
 
 def save_to_json(data, filename):
     os.makedirs("scraped_data", exist_ok=True)
@@ -22,9 +27,6 @@ def save_to_google_sheets(data, headers, sheet_name):
     if not data:
         print(" No data found to upload.")
         return
-
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("savedata-452919-69fd401a8a6d.json", scope)
     client = gspread.authorize(creds)
 
     try:
@@ -55,3 +57,20 @@ def save_to_google_sheets(data, headers, sheet_name):
 # test_data = [{"name": "(4 ELEMENTS FOR LIFE)", "image": "https://example.com/image.jpg", "brand_description": "Some description"}]
 # save_to_json(test_data,"Brand_sheet")
 
+def authenticate_drive():
+    """Authenticate using the existing service account credentials"""
+    return build("drive", "v3", credentials=creds)
+
+def upload_to_drive(file_path, folder_id=None):
+    
+    drive_service = authenticate_drive()
+    
+    file_metadata = {"name": os.path.basename(file_path)}
+    if folder_id:
+        file_metadata["parents"] = [folder_id]  
+
+    media = MediaFileUpload(file_path, mimetype="application/msword")
+    uploaded_file = drive_service.files().create(body=file_metadata, media_body=media, fields="id").execute()
+    
+    print(f"Uploaded {file_path} to Google Drive with file ID: {uploaded_file.get('id')}")
+    return uploaded_file.get("id")
